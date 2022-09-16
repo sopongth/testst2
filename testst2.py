eb3fb290f693a2c2eeb881515236d3934d31910a
sopongth <104857667+sopongth@users.noreply.github.com>
2022-09-16 23:59:39 +0700

    code v6

--- testst2.py
+++ testst2.py
@@ -99,7 +99,7 @@ def main():
 
             sound = st.empty()
             sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
-            time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
+            time.sleep(5)  # wait for 2 seconds to finish the playing of the audio
             sound.empty()  # optionally delete the element afterwards
         # Reset
         st.session_state["audio_buffer"] = pydub.AudioSegment.empty()
